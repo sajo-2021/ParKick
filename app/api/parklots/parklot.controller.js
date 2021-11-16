d477520e820bdf9cb928676800cfb67d3b27dca4
gunbbang001 <pik3735@naver.com>
2021-11-16 21:02:38 +0900

    20211116 db restruct18

--- app/api/parklots/parklot.controller.js
+++ app/api/parklots/parklot.controller.js
@@ -72,12 +72,12 @@ exports.deleteid = (req, res) => {
 exports.inclike = (req, res) => {
     Parklot.findById(req.body.lotid, "rate").then(lot => {
         console.log('rateid : ' + lot.rate);
-        Rate.incLike(lot.rate).then(rate => {
-            console.log('rate.inclike의 리턴 : '+rate);
-        }).catch(err => res.status(500).send(err));
-        // User.incLike(req.body.userid, req.body.lotid, req.body.pmt)
-        //     .then()
-        //     .catch(err => res.status(500).send(err));
+        // Rate.incLike(lot.rate).then(rate => {
+        //     console.log('rate.inclike의 리턴 : '+rate);
+        // }).catch(err => res.status(500).send(err));
+        User.incLike(req.body.userid, req.body.lotid, req.body.pmt)
+            .then()
+            .catch(err => res.status(500).send(err));
     }).catch(err => res.status(500).send(err));
 }
 
