ee7d4e07d91419ce734b04d7c2fcb3e105e44816
gunbbang001 <pik3735@naver.com>
2021-12-10 02:01:28 +0900

    20211210 login update 2

--- app/api/auth/auth.controller.js
+++ app/api/auth/auth.controller.js
@@ -1,6 +1,8 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../../models/user');
 /*
-    POST /auth
+    POST /auth/register
     {
         id,
         pwd,
@@ -37,4 +39,67 @@ exports.register = (req, res) => {
     .then(create)
     .then(respond)
     .catch(onError);
+}
+
+/*
+    POST /auth/login
+    {
+        id,
+        pwd
+    }
+*/
+
+exports.login = (req, res) => {
+    const {id, pwd} = req.body;
+    const secret = req.app.get('jwt-secret')
+
+    const check = (user) => {
+        if(!user){
+            throw new Error('login failed');
+        }else{
+            if(user.verify(password)){
+                const p = new Promise((resolve, reject) => {
+                    jwt.sign(
+                        {
+                            _id: user._id,
+                            id: user.id,
+                            name: user.name,
+                            nickname: user.nickname,
+                            email: user.email
+                        },
+                        secret,
+                        {
+                            expiresIn: '7d',
+                            issuer:'velopert.com',
+                            subject: 'userInfo'
+                        }, (err, token) => {
+                            if(err) reject(err);
+                            resolve(token);
+                        }
+                    );
+                })
+                return p;
+            }else{
+                throw new Error('login failed');
+            }
+        }
+    }
+
+    const respond = (token) => {
+        res.send({
+            message: 'logged in successfully',
+            token
+        });
+    }
+
+    const onError = (err) => {
+        res.status(403).send({
+            err: err.message
+        })
+    }
+
+    User.findOneByUserid(id)
+    .then(check)
+    .then(respond)
+    .catch(onError)
 }
