649b8bca049e1fcd58564a92455f8dd23a4287e4
gunbbang001 <pik3735@naver.com>
2021-12-10 02:30:48 +0900

    20211210 login update 5

--- app/api/auth/auth.controller.js
+++ app/api/auth/auth.controller.js
@@ -103,3 +103,41 @@ exports.login = (req, res) => {
     .then(respond)
     .catch(onError)
 }
+
+/*
+    GET /auth/check
+*/
+
+exports.check = (req, res) => {
+    const token = req.headers['x-access-token'] || req.query.token
+
+    if(!token){
+        return res.status(403).send({
+            success: false,
+            message: 'not logged in'
+        })
+    }
+
+    const p = new Promise((resolve, reject) => {
+        jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
+            if(err) reject(err)
+            resolve(decoded)
+        })
+    })
+
+    const respond = (token) => {
+        res.send({
+            success: true,
+            info: token
+        })
+    }
+
+    const onError = (err) => {
+        res.status(403).send({
+            success: false,
+            err: err.message
+        })
+    }
+
+    p.then(respond).catch(onError)
+}
